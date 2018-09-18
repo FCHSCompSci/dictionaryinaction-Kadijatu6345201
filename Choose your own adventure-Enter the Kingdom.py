40444bff0b62dce3fe821ddce42478e7bb3aaecf
Kadijatu Idy Tal <kadijatu.tal@stu.jefferson.kyschools.us>
2018-09-18 08:01:29 -0400

    Choose your own adventure

---
+++ Choose your own adventure-Enter the Kingdom.py
@@ -0,0 +1,11 @@
+import random
+
+game_stats = {
+    "health": 100,
+    "weapons": ["Staff", "Knife", "Scythe", "Scitars", "Claws"],
+    "xp": 0,
+    "kingdom_xp": 123564,
+    }
+
+def player_name (first, last):
+    return "Welcome, player before we begin, please enter your name: %s" %s(first,last)
