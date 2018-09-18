516ba217902e891ebbaed1d47d42750795cea794
Kadijatu Idy Tal <kadijatu.tal@stu.jefferson.kyschools.us>
2018-09-18 08:30:39 -0400

    began player input

--- Choose your own adventure-Enter the Kingdom.py
+++ Choose your own adventure-Enter the Kingdom.py
@@ -1,11 +1,15 @@
 import random
 
 game_stats = {
-    "health": 100,
-    "weapons": ["Staff", "Knife", "Scythe", "Scitars", "Claws"],
-    "xp": 0,
-    "kingdom_xp": 123564,
+    "level_up": 350,
+    "kingdom_coins": 10000,
     }
+STATS = {
+    "Level": 1,
+    "Weapon": "knife" ,
+    "xp": 0,
+    "Coins": 0,
+    }
+player = input ("Welcome, Player, to Golden Castle. Before we begin, may I know what to call you?: ")
 
-def player_name (first, last):
-    return "Welcome, player before we begin, please enter your name: %s" %s(first,last)
+print ("Alright then, " + player + ". Currently, you are in the trial zone, outside the kingdom walls of Avaral. To get into the kingdom, you need to get 10,000 coins. You currently have no coins. you can check on your coins, xp, and level by typing STATS. Give it a try: ")
