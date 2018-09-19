a92899dbb125a552babc768a53da847329dc312d
Kadijatu Idy Tal <kadijatu.tal@stu.jefferson.kyschools.us>
2018-09-19 08:30:25 -0400

    Began adding monsters

--- Choose your own adventure-Enter the Kingdom.py
+++ Choose your own adventure-Enter the Kingdom.py
@@ -1,15 +1,37 @@
 import random
 
 game_stats = {
-    "level_up": 350,
+    "level_up": 4,567,
     "kingdom_coins": 10000,
     }
 STATS = {
     "Level": 1,
-    "Weapon": "knife" ,
+    "Weapon": "knife",
     "xp": 0,
     "Coins": 0,
     }
+monster = {
+    "Nyarlathotep" == {
+        "xp": 253,
+        "coins": 534, 
+        }
+    "Azathoth" == {
+        "xp": 523780,
+        "coins": 4367926289,
+        }
+    "Dagon" == {
+        "xp": 52,
+        "coins": 36
+        }
+    "Hastur" == {
+        "xp": 456
+        "coins": 154
+        
+    }
 player = input ("Welcome, Player, to Golden Castle. Before we begin, may I know what to call you?: ")
 
-print ("Alright then, " + player + ". Currently, you are in the trial zone, outside the kingdom walls of Avaral. To get into the kingdom, you need to get 10,000 coins. You currently have no coins. you can check on your coins, xp, and level by typing STATS. Give it a try: ")
+print("Alright then, " + player + ". Currently, you are in the trial zone, outside the kingdom walls of Avaral. To get into the kingdom, you need to get 10,000 coins. You currently have no coins. you can check on your coins, xp, and level by typing STATS. To get more coins and xp, you need to fight the monsters in the forest around the perameter of the kingdom. Walk through the forest to come across random monsters. Each monster gives different amounts of coins and xp. Lets go find your first monster!")
+
+cmd = input("what is your command")
+if cmd != what i want them to do:
+    continue
